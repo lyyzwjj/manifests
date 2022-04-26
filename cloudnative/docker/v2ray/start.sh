44ec9f4e040d9aaf99e343f833d4e200a0941fb2
wjj <wzzst310@163.com>
2022-04-26 12:21:26 +0800

    修改

---
+++ cloudnative/docker/v2ray/start.sh
@@ -0,0 +1,1 @@
+sudo docker run -d --rm --name v2ray -p 443:443 -p 80:80 -v $HOME/.caddy:/root/.caddy  pengchujin/v2ray_ws:0.11 wjjzst.xyz V2RAY_WS 0890b53a-e3d4-4726-bd2b-52574e8588c4 && sleep 3s && sudo docker logs v2ray
